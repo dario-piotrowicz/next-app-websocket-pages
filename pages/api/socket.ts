e3ebbeda00410002043d722a7646ae0390fb2c5e
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-04-08 16:22:37 +0100

    fix types

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,7 +1,5 @@
 // Next.js Edge API routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
-import { Response, WebSocketPair } from '@cloudflare/workers-types';
-
 export const config = {
   runtime: "experimental-edge",
 };
@@ -10,7 +8,7 @@ export default function handler() {
   const webSocketPair = new WebSocketPair();
   const [client, server] = Object.values(webSocketPair);
 
-  server.accept();
+  (server as WebSocket & { accept: () => void }).accept();
 
   server.addEventListener('message', event => {
     const message = event.data;
